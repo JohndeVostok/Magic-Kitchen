b06019fc55439637968fd446cf30329243a7e32b
wys <wangyisong1996@126.com>
2017-10-03 22:25:43 +0800

    Add static files serving in Django

--- CodeCheF/settings.py
+++ CodeCheF/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2^*390ig+msvw7@*9hgm6^1nra^7bu-a$5n6#2b2+mrpd(y^m6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "client"),
+]
+
+STATIC_ROOT = ''
