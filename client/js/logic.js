61014331035d54cc7474d2a6c9721e912a5b8872
JohndeVostok <dyxdy@live.com>
2017-10-14 14:52:51 +0800

    client.logic: Add some basic and atomic functions.
    Change mapGen to stateGen.
    Add reset, step, moveForward, rotate functions.

--- client/js/logic.js
+++ client/js/logic.js
@@ -1,23 +1,88 @@
 var logic = function()
 {
-	var mapGen = function()
+	var stateGen = function()
 	{
-		map = new Array(169);
+		var map = new Array(config.mapWidth * config.mapHeight);
+		var positionX = 0, positionY = 0, direction = 0;
 
+		var moveForward() = function()
+		{
+			switch(direction)
+			{
+				case 0:
+				//down
+					if (positionY + 1 < config.mapHeight) positionY++; else envalidOp;
+				break;
+				case 1:
+				//right
+					if (positionX + 1 < config.mapWidth) positionX++; else envalidOp;
+				break;
+				case 2:
+				//up
+					if (positionY > 0) positionY--; else envalidOp;
+				break;
+				case 3:
+				//left
+					if (positionX > 0) positionX--; else envalidOp;
+				break;
+			}
+		};
+
+		var rotate = function(dir)
+		{
+			direction = (direction + dir) % 4;
+		}
+
+		return
+		{
+			map: map;
+			positionX: positionX;
+			positionY: positionY;
+			direction: direction;
+			moveForward: moveForward;
+			rotate: rotate;
+		};
 	};
 
-	var map = mapGen();
-	var originMap = mapGen();
+	var currentState = stateGen();
+	var originState = stateGen();
 
 	var initMap = function()
 	{
-		for (var i = 0; i < 13; i++) for (var j = 0; j < 13; j++) map[13 * i + j] = 0;
+		for (var i = 0; i < config.mapWidth; i++) for (var j = 0; j < config.mapHeight; j++) currentState.map[13 * i + j] = 0;
+//		ui.loadMap(levelData);
+//		ui.loadUserInfo("");
 	};
-	
-	var doLoad = function() {};
+
+	var doLoad = function()
+	{
+	};
+
+	var loadLevel = function()
+	{
+		initMap();//tem test without network
+//		network.fetchLevel(initMap);
+	};
 	
 	var reset = function()
 	{
+	};
+
+	var envalidOP = function()
+	{
+		console.log("out");
+	};
+
+	var singleStepForward = function()
+	{
+		state.moveForward();
+		console.log("fo");
+	};
+
+	var rotate = function(dir)
+	{
+		state.rotate(dir);
+		console.log("dir");
 	};
 
 	var step = function(op)
@@ -29,9 +94,11 @@ var logic = function()
 			break;
 			case 1:
 			//singleStepForward
+				singleStepForward();
 			break;
 			case 2:
 			//rotate
+				rotate(op["info"]);
 			break;
 			case 3:
 			//load
