c5298b69316c98bf21311de24f9d25871e34c0e8
2015011299 <hq15@mails.tsinghua.edu.cn>
2017-10-16 22:10:36 +0800

    client.code: Add basic blocks. (#27)

--- client/js/code.js
+++ client/js/code.js
@@ -1,12 +1,46 @@
 var code = function() {
 	var doLoad = function() {
+		this.setBlockTypes([0, 1, 3]);
+	};
+
+	var setBlockTypes = function(blockIDList) {
+		for (var id=0; id<blockIDList.length; id++)
+		{
+			var blockDef = config.blocks[blockIDList[id]];
+
+			Blockly.Blocks[blockDef.name] = function(blockDef){
+				var init = function() {
+					this.jsonInit(blockDef.json);
+					blockDef.initExtra(this);
+				};
+				return {
+					init: init
+				};
+			}(blockDef);
+
+			Blockly.JavaScript[blockDef.name] = function(blockDef) {
+				return function(block) {
+					return blockDef.generateJavaScript(block);
+				}
+			}(blockDef);
+
+			var newBlock = $("<block type=\"" + blockDef.name + "\"></block>")
+			$("xml#toolbox").append(newBlock);
+		}
 		workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox')});
+	};
+
+	var test = function() {
+		var code = Blockly.JavaScript.workspaceToCode(workspace);
+		$("p#test").text(code);
 	};
 	
 	// Blockly workspace
 	var workspace;
 	
 	return {
-		doLoad: doLoad
+		doLoad: doLoad,
+		setBlockTypes: setBlockTypes,
+		test: test
 	};
 }();
--- client/js/config.js
+++ client/js/config.js
@@ -1,1 +1,81 @@
-var config = {mapWidth: 13, mapHeight: 13};
+var config = {
+	mapWidth: 13,
+	mapHeight: 13,
+
+	blocks: [
+		{
+			name: "single_step_forward",
+			json: {
+				"message0": "向前一步",
+				"tooltip": "按照当前方向向前行进一步",
+				"previousStatement": null,
+				"nextStatement": null,
+				"colour": 120
+			},
+			initExtra: function(block){},
+			generateJavaScript: function(block){
+				op = {"typeID": 1};
+				return "logic.step(" + JSON.stringify(op) + ");";
+			}
+		},
+		{
+			name: "rotate",
+			json: {
+				"message0": "旋转： %1",
+				"tooltip": "以当前方向为基准进行转向",
+				"args0": [
+					{
+						"type": "field_dropdown",
+						"name": "ANGLE",
+						"options": [
+							[ "逆时针90度", "1" ],
+							[ "180度", "2" ],
+							[ "顺时针90度", "3" ]
+						]
+					}
+				],
+				"previousStatement": null,
+				"nextStatement": null,
+				"colour": 300
+			},
+			initExtra: function(block){},
+			generateJavaScript: function(block){
+				op = {
+					"typeID": 2,
+					"dir": parseInt(block.getFieldValue("ANGLE"))
+				};
+				return "logic.step(" + JSON.stringify(op) + ");";
+			}
+		},
+		{
+			name: "pick_up",
+			json: {
+				"message0": "拿起物品",
+				"tooltip": "将所在位置的操作台上的物品拿到头顶",
+				"previousStatement": null,
+				"nextStatement": null,
+				"colour": 180
+			},
+			initExtra: function(block){},
+			generateJavaScript: function(block){
+				op = {"typeID": 3};
+				return "logic.step(" + JSON.stringify(op) + ");";
+			}
+		},
+		{
+			name: "put_down",
+			json: {
+				"message0": "放下物品",
+				"tooltip": "将头顶的物品放置到所在位置的操作台上",
+				"previousStatement": null,
+				"nextStatement": null,
+				"colour": 240
+			},
+			initExtra: function(block){},
+			generateJavaScript: function(block){
+				op = {"typeID": 4};
+				return "logic.step(" + JSON.stringify(op) + ");";
+			}
+		}
+	]
+};
