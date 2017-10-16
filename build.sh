b8fce835520b3caff30269321d25173b6a7c86e4
wys <wangyisong1996@126.com>
2017-10-16 10:24:37 +0800

    tools: Update build.sh. (#23)

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+# This file is used for SEP (Software Engineering Platform)
+# DON'T use it in production!!
+
+# Remove the SQLite DB first
+rm -f db.sqlite3
+# Run Django migration
+python manage.py migrate
+# Run tests
 coverage run manage.py test
+# Generate coverage reports
+rm -rf Coverage_Python
 coverage html -d Coverage_Python
 
