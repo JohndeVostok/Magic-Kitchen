0303ab2051018b720b3c504d70a8059c8f08f9f4
wys <wangyisong1996@126.com>
2017-10-17 22:50:48 +0800

    client.ui: Implement animation functions.(#20,#21)

--- client/js/ui.js
+++ client/js/ui.js
@@ -2,6 +2,9 @@ var ui = function() {
 	var doLoad = function() {
 		loadStage();
 		
+		loadConfig();
+		initUI();
+		
 		// Test CreateJS and Blockly
 		
 		for (var i = 0; i < 1; i++) {
@@ -55,12 +58,61 @@ var ui = function() {
 		stage.update();
 	};
 	
+	var N, M;
+	
+	var loadConfig = function() {
+		N = config.mapHeight;
+		M = config.mapWidth;
+	};
+	
+	// Map state
+	var map = [];
+	var mapSize;
+	
+	// Item states
+	var items = [];
+	var itemOnHead;
+	
+	// Player state
+	var playerX, playerY;
+	
+	// Animation queue
+	var animationQueue = [];
+	
+	var initUI = function() {
+		// Init map.
+		mapSize = N * M;
+		for (var i = 0; i < mapSize; i++) {
+			map[i] = 0;
+		}
+		
+		// Init player.
+		playerX = 0;
+		playerY = 0;
+		
+		// Init animation queue.
+		animationQueue = [];
+		
+		// Init items.
+		// Use clear items animation.
+		// Animation queue must be initialized before this.
+		clearItems();
+	};
+	
 	var start = function() {
 		// TODO: implement this after logic is implemented
 	};
 	
 	var loadMap = function(mapData) {
+		if (!mapData || !mapData.length || mapData.length != mapSize) {
+			throw "Invalid mapData";
+		}
 		
+		for (var i = 0; i < mapSize; i++) {
+			var x = mapData[i];
+			if (isNaN(x)) throw "Invalid mapData[" + i + "]: " + x;
+			map[i] = parseInt(x);
+		}
 	};
 	
 	var loadUserInfo = function(userInfo) {
@@ -71,34 +123,125 @@ var ui = function() {
 		// TODO: implement this
 	};
 	
+	// Common functions
+	
+	// Check for valid map position. Throws an error on invalid pos.
+	// Returns: Integer type of pos.
+	var checkValidMapPos = function(pos) {
+		if (isNaN(pos)) {
+			throw "Invalid pos " + pos;
+		}
+		pos = parseInt(pos);
+		if (pos < -1 || pos >= mapSize) {
+			throw "Invalid pos " + pos;
+		}
+		return pos;
+	};
+	
+	// Check for valid item position. Throws an error on invalid pos.
+	// Returns: Integer type of pos.
+	var checkValidItemPos = function(pos) {
+		if (isNaN(pos)) {
+			throw "Invalid pos " + pos;
+		}
+		pos = parseInt(pos);
+		if (pos < 0 || pos >= mapSize) {
+			throw "Invalid pos " + pos;
+		}
+		return pos;
+	}
+	
+	// Check for valid direction (0,1,2,3). Throws an error on invalid direction.
+	// Returns: Integer type of dir.
+	var checkValidDirection = function(dir) {
+		if (isNaN(dir)) {
+			throw "Invalid direction " + dir;
+		}
+		dir = parseInt(dir);
+		if (dir < 0 || dir >= 4) {
+			throw "Invalid direction " + dir;
+		}
+		return dir;
+	};
+	
 	// Below are animation functions, i.e. functions that register animations for later rendering.
 	
 	var clearItems = function() {
-		
+		animationQueue.push({
+			type: "clearItems"
+		});
+		for (var i = 0; i < mapSize; i++) {
+			items[i] = undefined;
+		}
+		itemOnHead = undefined;
 	};
 	
 	var newItem = function(pos, type, args) {
+		pos = checkValidItemPos(pos);
 		
+		if (isNaN(type)) {
+			throw "Invalid type";
+		}
+		type = parseInt(type);
+		
+		animationQueue.push({
+			type: "newItem",
+			args: {
+				pos: pos,
+				type: type,
+				args: args
+			}
+		});
 	};
 	
 	var deleteItem = function(pos, args) {
+		pos = checkValidItemPos(pos);
 		
+		animationQueue.push({
+			type: "deleteItem",
+			args: {
+				pos: pos
+			}
+		});
 	};
 	
 	var addAnimation = function(pos1, pos2, args) {
+		pos1 = checkValidItemPos(pos1);
+		pos2 = checkValidItemPos(pos2);
 		
+		animationQueue.push({
+			type: "addAnimation",
+			args: {
+				pos1: pos1,
+				pos2: pos2,
+				args: args
+			}
+		});
 	};
 	
 	var addPlayerAnimation = function(pos1, pos2, dir1, dir2) {
+		pos1 = checkValidMapPos(pos1);
+		pos2 = checkValidMapPos(pos2);
+		dir1 = checkValidDirection(dir1);
+		dir2 = checkValidDirection(dir2);
 		
+		animationQueue.push({
+			type: "addPlayerAnimation",
+			args: {
+				pos1: pos1,
+				pos2: pos2,
+				dir1: dir1,
+				dir2: dir2
+			}
+		});
 	};
 	
 	var setInput = function(itemList) {
-		
+		// TODO
 	};
 	
 	var setOutput = function(itemList) {
-		
+		// TODO
 	};
 	
 	return {
