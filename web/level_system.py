3f40a6c74d6f95e45626a3eaf8640e76c03cc325
sth <765215342@qq.com>
2017-10-26 23:44:07 +0800

    back-end.web.*:Change Level.level_id in level_system.py.(#74)

--- web/level_system.py
+++ web/level_system.py
@@ -14,12 +14,6 @@ def int_range(x):
 def get_session(request):
     return request.session.get('name', None)
 
-def default_level_id_range(x):
-    if x >=0 and x <= 100:
-        return True
-    else:
-        return False
-
 def new_default_level(request):
     #TODO
     #only admin can create new default level
@@ -28,8 +22,8 @@ def new_default_level(request):
     ret = {}
     ret['status'] = 'failed'
 
-    if not 'level_id' in content:
-        ret['error'] = 'level id can\'t be empty'
+    if not 'default_level_id' in content:
+        ret['error'] = 'default level id can\'t be empty'
         return json_response(ret)
 
     if not 'level_info' in content:
@@ -37,56 +31,74 @@ def new_default_level(request):
         return json_response(ret)
 
     try:
-        _id = int(content['level_id'])
+        _id = int(content['default_level_id'])
     except ValueError,e :
         print e
-        ret['error'] = 'the input level id needs to be an Integer'
+        ret['error'] = 'the input default level id needs to be an Integer'
         return json_response(ret)
 
-    #0~100 are default levels, others are user-made levels
-    if not default_level_id_range(_id):
-        ret['error'] = 'the input level id needs to be in range [0,100]'
-        return json_response(ret)
-
-    level_id_filter = Level.objects.filter(level_id = _id)
-    if len(level_id_filter) > 0:
-        ret['error'] = 'this level id already exists'
+    default_level_id_filter = Level.objects.filter(default_level_id = _id)
+    if len(default_level_id_filter) > 0:
+        ret['error'] = 'this default level id already exists'
         return json_response(ret)
 
     _info = content['level_info']
-    Level.objects.create(level_id = _id, info = _info, user_name = 'admin')
+    Level.objects.create(default_level_id = _id, info = _info, user_name = 'admin')
 
     ret['status'] = 'succeeded'
 
     return json_response(ret)
 
 def get_level_info(request):
+    #you can use default_level_id to get default level or level_id to get any level(default or user-made)
     content = request.POST
     ret = {}
     ret['status'] = 'failed'
 
-    if not 'level_id' in content:
-        ret['error'] = 'level id can\'t be empty'
+    if not (('level_id' in content) or ('default_level_id' in content)):
+        ret['error'] = 'level id and default level id can\'t be empty in the same time'
         return json_response(ret)
 
-    try:
-        _id = int(content['level_id'])
-    except ValueError,e :
-        print e
-        ret['error'] = 'the input level id needs to be an Integer'
-        return json_response(ret)
+    if 'default_level_id' in content:
+        #get default level
+        try:
+            _default_level_id = int(content['default_level_id'])
+        except ValueError,e :
+            print e
+            ret['error'] = 'the input default level id needs to be an Integer'
+            return json_response(ret)
 
-    if not int_range(_id):
-        ret['error'] = 'the input level id needs to be an Integer'
-        return json_response(ret)
+        if not int_range(_id):
+            ret['error'] = 'the input default level id needs to be an Integer'
+            return json_response(ret)
 
-    level_id_filter = Level.objects.filter(level_id = _id)
-    if len(level_id_filter) == 0:
-        ret['error'] = 'this level doesn\'t exist'
-        return json_response(ret)
+        default_level_id_filter = Level.objects.filter(default_level_id = _default_level_id)
+        if len(default_level_id_filter) == 0:
+            ret['error'] = 'this level doesn\'t exist'
+            return json_response(ret)
 
-    ret['status'] = 'succeeded'
-    ret['level_info'] = level_id_filter[0].info #json
+        ret['status'] = 'succeeded'
+        ret['level_info'] = default_level_id_filter[0].info #json
+
+    else:
+        try:
+            _level_id = int(content['level_id'])
+        except ValueError,e :
+            print e
+            ret['error'] = 'the input level id needs to be an Integer'
+            return json_response(ret)
+
+        if not int_range(_id):
+            ret['error'] = 'the input level id needs to be an Integer'
+            return json_response(ret)
+
+        level_id_filter = Level.objects.filter(level_id = _level_id)
+        if len(level_id_filter) == 0:
+            ret['error'] = 'this level doesn\'t exist'
+            return json_response(ret)
+
+        ret['status'] = 'succeeded'
+        ret['level_info'] = level_id_filter[0].info #json
 
     return json_response(ret)
 
@@ -107,7 +119,7 @@ def new_usermade_level(request):
         return json_response(ret)
 
     _info = content['level_info']
-    Level.objects.create(level_id = -1, info = _info, user_name = session)
+    Level.objects.create(default_level_id = -1, info = _info, user_name = session)
 
     ret['status'] = 'succeeded'
 
