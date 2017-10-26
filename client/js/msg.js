b86666ce4784b95596790fe5e0b4e8f39900804e
mzx <mazx15@mails.tsinghua.edu.cn>
2017-10-26 22:47:51 +0800

    client.message: Refactor msg.content. (#72)

--- client/js/msg.js
+++ client/js/msg.js
@@ -1,21 +1,18 @@
 function Msg()
 {
 	var content = {
-		logic: [
-			["ERROR", "ERROR"],
-			["Target is out of map.", "目标超出地图边界。"],
-			["Target is an operation floor.", "目标是操作台。"],
-			["Invalid address.", "地址不存在"],
-			[],[],[],[],[],[],[],
-			["Target is not an operation floor.", "目标不是操作台。"],
-			["I have something in my hand.", "我手里有东西。"],
-			["There is nothing there to load", "那没什么可拿的。"],
-			["Inbox in empty.", "进材口已经空了。"],
-			["I have nothing in my hand.", "我手里没东西。"],
-			["There is something there.", "那已经有东西了。"],
-			["I can't store to there.", "我不能向进材口丢东西。"],
-			["It's not what we want.", "那个不是我们想要的。"]
-		]
+		3000: ["ERROR", "ERROR"],
+		3001: ["Target is out of map.", "目标超出地图边界。"],
+		3002: ["Target is an operation floor.", "目标是操作台。"],
+		3003: ["Invalid address.", "地址不存在"],
+		3011: ["Target is not an operation floor.", "目标不是操作台。"],
+		3012: ["I have something in my hand.", "我手里有东西。"],
+		3013: ["There is nothing there to load", "那没什么可拿的。"],
+		3014: ["Inbox in empty.", "进材口已经空了。"],
+		3015: ["I have nothing in my hand.", "我手里没东西。"],
+		3016: ["There is something there.", "那已经有东西了。"],
+		3017: ["I can't store to there.", "我不能向进材口丢东西。"],
+		3018: ["It's not what we want.", "那个不是我们想要的。"]
 	}
 	var language = 0;
 
@@ -37,11 +34,10 @@ function Msg()
 
 	this.getMessage = function(msgId)
 	{
-		if (3000 <= msgId && msgId < 4000)
-		{
-			return content.logic[msgId - 3000][language];
-		}
-		return undefined;
+		var str = content.logic[msgId][language];
+		if (str == undefined)
+			str = "INVALID MSG CODE";
+		return str;
 	};
 }
 
